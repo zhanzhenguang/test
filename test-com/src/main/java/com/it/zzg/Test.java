b55abbed73d5ee66fb39698b0e772600cfea98e1
zhanzhenguang <115071716@qq.com>
2019-02-20 17:32:19 +0800

    阿萨德

--- test-com/src/main/java/com/it/zzg/Test.java
+++ test-com/src/main/java/com/it/zzg/Test.java
@@ -4,5 +4,11 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println(11123);
+		for (int i = 0; i <10; i++) {
+			System.out.println(i);
+			System.out.println(11);
+			System.out.println(11);
+		}
+		
 	}
 }
